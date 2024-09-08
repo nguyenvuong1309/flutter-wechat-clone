2e5ff785bf368198fae5521c94514f073332a706
nguyenvuong1309 <nguyenducvuong13092003@gmail.com>
2024-09-08 09:56:32 +0700

    add cache video player plus

--- lib/widgets/video_player_widget.dart
+++ lib/widgets/video_player_widget.dart
@@ -1,79 +1,95 @@
-// import 'package:cached_video_player/cached_video_player.dart';
-// import 'package:flutter/material.dart';
+import 'package:cached_video_player_plus/cached_video_player_plus.dart';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-// class VideoPlayerWidget extends StatefulWidget {
-//   const VideoPlayerWidget({
-//     super.key,
-//     required this.videoUrl,
-//     required this.color,
-//     required this.viewOnly,
-//   });
+class VideoPlayerWidget extends StatefulWidget {
+  const VideoPlayerWidget({
+    super.key,
+    required this.videoUrl,
+    required this.color,
+    required this.viewOnly,
+  });
 
-//   final String videoUrl;
-//   final Color color;
-//   final bool viewOnly;
+  final String videoUrl;
+  final Color color;
+  final bool viewOnly;
 
-//   @override
-//   State<VideoPlayerWidget> createState() => _VideoPlayerWidgetState();
-// }
+  @override
+  State<VideoPlayerWidget> createState() => _VideoPlayerWidgetState();
+}
 
-// class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
-//   late CachedVideoPlayerController videoPlayerController;
-//   bool isPlaying = false;
-//   bool isLoading = true;
+class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
+  late CachedVideoPlayerPlusController videoPlayerPlusController;
+  late VideoPlayerController _controller;
 
-//   @override
-//   void initState() {
-//     videoPlayerController = CachedVideoPlayerController.network(
-//       widget.videoUrl,
-//     )
-//       ..addListener(() {})
-//       ..initialize().then((_) {
-//         videoPlayerController.setVolume(1);
-//         setState(() {
-//           isLoading = false;
-//         });
-//       });
-//     super.initState();
-//   }
+  bool isPlaying = false;
+  bool isLoading = true;
+  double ratio = 1;
 
-//   @override
-//   void dispose() {
-//     videoPlayerController.dispose();
-//     super.dispose();
-//   }
+  @override
+  void initState() {
+    super.initState();
+    videoPlayerPlusController = CachedVideoPlayerPlusController.networkUrl(
+      Uri.parse(
+        widget.videoUrl,
+      ),
+      httpHeaders: {
+        'Connection': 'keep-alive',
+      },
+      invalidateCacheIfOlderThan: const Duration(days: 1),
+    )..initialize().then((value) async {
+        videoPlayerPlusController.play();
+        setState(() {});
+      });
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return AspectRatio(
-//       aspectRatio: 16 / 9,
-//       child: Stack(
-//         children: [
-//           isLoading
-//               ? const Center(
-//                   child: CircularProgressIndicator(),
-//                 )
-//               : CachedVideoPlayer(videoPlayerController),
-//           Center(
-//             child: IconButton(
-//               icon: Icon(
-//                 isPlaying ? Icons.pause : Icons.play_arrow,
-//                 color: widget.color,
-//               ),
-//               onPressed: widget.viewOnly
-//                   ? null
-//                   : () {
-//                       setState(() {
-//                         isPlaying = !isPlaying;
-//                         isPlaying
-//                             ? videoPlayerController.play()
-//                             : videoPlayerController.pause();
-//                       });
-//                     },
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
+    _controller = VideoPlayerController.networkUrl(Uri.parse(
+      widget.videoUrl,
+    ))
+      ..initialize().then((_) {
+        setState(() {
+          ratio = _controller.value.aspectRatio;
+          isLoading = false;
+        });
+      });
+  }
+
+  @override
+  void dispose() {
+    videoPlayerPlusController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: ratio,
+      child: Stack(
+        children: [
+          isLoading
+              ? const Center(
+                  child: CircularProgressIndicator(),
+                )
+              : CachedVideoPlayerPlus(videoPlayerPlusController),
+          Center(
+            child: IconButton(
+              icon: Icon(
+                isPlaying ? Icons.pause : Icons.play_arrow,
+                color: widget.color,
+              ),
+              onPressed: widget.viewOnly
+                  ? null
+                  : () {
+                      setState(() {
+                        isPlaying = !isPlaying;
+                        isPlaying
+                            ? videoPlayerPlusController.play()
+                            : videoPlayerPlusController.pause();
+                      });
+                    },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
